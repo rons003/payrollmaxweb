ed04229a6c68c0227ef3ddbf1d97bf40065d5df9
rons003 <ronelle003@gmail.com>
2018-12-19 10:43:33 +0800

    Scaffold Database

---
+++ Models/Reports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace payroll.Models
+{
+    public partial class Reports
+    {
+        public int Id { get; set; }
+        public string ReportName { get; set; }
+        public string Path { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
